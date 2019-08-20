60f995e3eef234f15b7df5c6e2dbfb7f2595bd41
akbarlaz <akbar.lazuardi@yahoo.com>
2019-08-20 13:39:36 +0700

    add novice folder

---
+++ novice/01-01/latihan/VariabelApp/VariabelApp.java
@@ -0,0 +1,15 @@
+public class VariabelApp {
+    /* public void pupAge() {
+        int age = 0;
+        age = age + 7;
+        System.out.println("Puppy age is : "+ age);
+    } */
+
+    public static void main(String[] args){
+        /* VariabelApp variabel = new VariabelApp();
+        variabel.pupAge(); */
+        int age = 0;
+        age = age + 7;
+        System.out.println("Puppy age is : "+ age);
+    }
+}
