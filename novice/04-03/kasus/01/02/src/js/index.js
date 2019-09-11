b9097e6d0fc00d9cd54c15ec76030d1f674cb818
akbarlaz <akbar.lazuardi@yahoo.com>
2019-09-11 11:48:40 +0700

    add kasus

---
+++ novice/04-03/kasus/01/02/src/js/index.js
@@ -0,0 +1,33 @@
+var table = document.querySelector('#shoppingList');
+var input = document.querySelector('input');
+var button = document.querySelector('button');
+
+button.onclick = function () {
+    var inputValue = input.value;
+    input.value = '';
+    table.className = "table is-striped";
+
+    var tableRow = document.createElement('tr');
+    var tableColumn = document.createElement('td');
+    tableColumn.textContent = inputValue;
+    var tableColumn2 = document.createElement('td');
+    var listBtn = document.createElement('button');
+    listBtn.textContent = "DELETE"
+    listBtn.className += " button is-danger is-small";
+
+    
+    table.appendChild(tableRow);
+    tableRow.appendChild(tableColumn);
+    tableRow.appendChild(tableColumn2);
+    tableColumn2.appendChild(listBtn);
+
+    listBtn.onclick = function(e) {
+        table.removeChild(tableRow);
+        if (table.childElementCount == 1) {
+            table.className += " is-hidden"
+        }
+    }
+
+    input.focus();
+    
+}
