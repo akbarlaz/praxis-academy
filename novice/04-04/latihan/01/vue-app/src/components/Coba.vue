9130891cf17babc11afcee518c1b89e45a0e38ca
akbarlaz <akbar.lazuardi@yahoo.com>
2019-09-12 13:53:13 +0700

    04-04

---
+++ novice/04-04/latihan/01/vue-app/src/components/Coba.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="coba">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Coba',
+    props: {
+    msg: String,
+    item: Object
+  },
+  data: function () {
+    return {
+      isOpen: false
+    }
+  },
+  computed: {
+    isFolder: function () {
+      return this.item.children &&
+        this.item.children.length
+    }
+  },
+  methods: {
+    toggle: function () {
+      if (this.isFolder) {
+        this.isOpen = !this.isOpen
+      }
+    },
+    makeFolder: function () {
+      if (!this.isFolder) {
+      	this.$emit('make-folder', this.item)
+        this.isOpen = true
+      }
+    }
+  }
+}
+</script>
---
+++ novice/04-04/latihan/01/vue-app/src/views/Coba.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="coba">
+        <Coba msg="Welcome to coba page"/>
+    </div>
+</template>
+
+<script>
+import Coba from '@/components/Coba.vue'
+
+export default {
+    name: 'coba',
+    components: {
+        Coba
+    }
+}
+
+var demo = new Vue({
+  el: '#demo',
+  data: {
+    coba: coba
+  },
+  methods: {
+  	makeFolder: function (item) {
+    	Vue.set(item, 'children', [])
+      this.addItem(item)
+    },
+    addItem: function (item) {
+    	item.children.push({
+        name: 'new stuff'
+      })
+    }
+  }
+})
+</script>
+
